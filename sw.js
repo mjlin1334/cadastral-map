4f7a4fd8d35e2d3f76cebc5a381892124b4fc43b
mjlin1334 <75101029+mjlin1334@users.noreply.github.com>
2026-09-21 16:18:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cadastral-v15-auth-test-20260921b';
+const CACHE='cadastral-v16-auth-test-20260921b';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim();})());});
 self.addEventListener('fetch',e=>{
